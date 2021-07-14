3c7825fbdec60775d5424e3deae7038be0ab6b28
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-07-14 13:40:42 -0700

    code changes to sql queries to work on northwind db

--- queries.sql
+++ queries.sql
@@ -6,23 +6,23 @@
     left join categories as c
     on p.productid = c.categoryid
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-    select o.orderid, o.orderdate, s.shippername --getting only 196 records
-    from orders as o
-    left join shippers as s
-    on o.orderid = s.shipperid
-    where orderdate < '2012-08-09'
+    select o.id, o.orderdate, s.companyname 
+    from [order] as o
+    left join shipper as s
+    on s.id = o.shipvia
+    where orderdate < '2012-08-09';
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 select p.productname, od.quantity
-    from products as p
-     join orderdetails as od 
-    on p.productid = od.productid
+    from product as p
+     join orderdetail as od 
+    on p.id = od.productid
       where od.orderid = 10251
 order by p.productname;
 
--- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
-select o.orderid, c.customername, e.lastname --getting only 196 records
-    from orders as o
-    left join customers as c
-    on o.orderid = c.customerid
-     left join employees as e
-    on o.orderid = e.employeeid
+-- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records. 
+   select o.id as orderid, c.companyname, e.lastname as employee_lastname
+    from [order] as o
+     join customer as c
+    on o.customerid = c.id
+      join employee as e
+    on o.employeeid = e.id
